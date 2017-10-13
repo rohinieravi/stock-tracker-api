07c35970cbe213425cf5daf69a1db0282f1d56c1
rohinieravi <rohinieravi@yahoo.com>
2017-10-13 11:02:13 -0700

    fixing issue

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 app.get('/api/*', (req, res) => {
 res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.exports = {app};
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8040;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
--- test/test-server.js
+++ test/test-server.js
@@ -1,19 +1,26 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe('API', function() {
+ before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
 
 it('should 200 on GET requests', function() {
- return chai.request(app)
-   .get('/api/fooooo')
-   .then(function(res) {
-     res.should.have.status(200);
-     res.should.be.json;
-   });
-});
+	 return chai.request(app)
+	   .get('/api/fooooo')
+	   .then(function(res) {
+	     res.should.have.status(200);
+	     res.should.be.json;
+	   });
+	});
 });
